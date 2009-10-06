d140d2084f6c69acee338bfb623436203e9e37d4
Magnus Enger <magnus@torfeus.deich.folkebibl.no>
2009-10-06 09:59:36 +0200

    Modulen for albuminfo fungerer nå.

--- api/mod.albuminfo.php
+++ api/mod.albuminfo.php
@@ -15,19 +15,25 @@ if (!empty($_GET['album']) && !empty($_GET['artist'])) {
 	$url .= "&api_key=" . $config['lastfm']['api_key'];
 	$url .= "&artist=" . urlencode(avinverter($_GET['artist']));
 	$url .= "&album=" . urlencode($_GET['album']);
+	$url .= "&format=json";
 	
-	if ($album = simplexml_load_file($url)) {
-		echo("<p>{$album->album->name}</p>");
-		
-		// echo($url);
-		// print_r($album->album);
-		
-		if ($album->album->image[2]) {
-			echo('<p class="albumbilde"><img src="' . $album->album->image[2] . '" alt="' . $album->album->name . '" title="' . $album->album->name . '" /></p>');
+	if ($album = json_decode(file_get_contents($url), true)) {
+		// Sjekk om det er noe feil
+		if ($album['error']) {
+			echo("<p>Beklager, det oppstod en feil!<br />({$album['message']})</p>");
+			exit;
 		}
-		if ($alb['wiki']['summary']) {
-			echo("<p>Beskrivelse fra Last.fm: {$alb['wiki']['summary']}</p>");
+		// Tittel
+		echo("<p>{$album['album']['name']}</p>");
+		// Bilde
+		if ($album['album']['image'][2]['#text']) {
+			echo('<p class="albumbilde"><img src="' . $album['album']['image'][2]['#text'] . '" alt="' . $album['album']['name'] . '" title="' . $album['album']['name'] . '" /></p>');
 		}
+		// Beskrivelse
+		if ($album['album']['wiki']['summary']) {
+			echo("<p>Beskrivelse fra Last.fm: {$album['album']['wiki']['summary']}</p>");
+		}
+	
 	}
 	
 }
