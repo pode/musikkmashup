44c3e83071e7d9b7b4076179858d7fe94c1604a9
Magnus Enger <magnus@torfeus.deich.folkebibl.no>
2009-10-06 11:42:44 +0200

    Legger til modul for DVDer

---
+++ api/mod.dvd.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once('../config.php');
+
+if (!$config['moduler']['dvd']['aktiv']) {
+  exit;
+}
+
+include_once('../include/functions.php');
+
+if (!empty($_GET['artist'])) {
+	
+	if ($config['libraries'][$_GET['bib']]['sru']) {
+			$q = masser_input($_GET['artist']);
+			$qu = urlencode($q);
+			$query = 'dc.author=' . $qu . '+and+dc.title=dvd';
+			echo(sru_search($query, $_GET['bib'], $config['moduler']['dvd']['antall'], true));
+	} else {
+		echo('<p>Z39.50 - kommer...</p>');	
+	}
+	
+}
+
+?>
