451d97e9a1ae37005da3235f2582f3a5f39cf5d1
Magnus Enger <magnus@torfeus.deich.folkebibl.no>
2009-10-20 11:35:13 +0200

    Endrer mod.dvd.php til å bruke modulsearch-funksjonen.

--- api/mod.dvd.php
+++ api/mod.dvd.php
@@ -10,14 +10,23 @@ include_once('../include/functions.php');
 
 if (!empty($_GET['artist'])) {
 	
+	$q = masser_input($_GET['artist']);
+	$query = '';
 	if ($config['libraries'][$_GET['bib']]['sru']) {
-			$q = masser_input($_GET['artist']);
-			$qu = urlencode($q);
-			$query = 'dc.author=' . $qu . '+and+dc.title=dvd';
-			echo(sru_search($query, $_GET['bib'], $config['moduler']['dvd']['antall'], true));
+		$qu = urlencode($q);
+		$query = "dc.author=$qu+and+dc.title=dvd";
 	} else {
-		echo('<p>Z39.50 - kommer...</p>');	
+		$query = "fo=$q and ti=dvd";
 	}
+
+	echo(modulsearch($query, 'dvd'));
+	
+//	if ($config['libraries'][$_GET['bib']]['sru']) {
+//			$query = 'dc.author=' . $qu . '+and+dc.title=dvd';
+//			echo(sru_search($query, $_GET['bib'], $config['moduler']['dvd']['antall'], true));
+//	} else {
+//		echo('<p>Z39.50 - kommer...</p>');	
+//	}
 	
 }
 
