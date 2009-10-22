469e908b49738b953db6272f9ff5a17e74520298
Magnus Enger <magnus@torfeus.deich.folkebibl.no>
2009-10-22 14:47:21 +0200

    Fikser visning av æøå i Explorer. Issue 2.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php echo('<?xml version="1.0" encoding="utf-8"?>'); ?>
-
 <?php 
+header('Content-Type: text/html; charset=utf-8');
+echo('<?xml version="1.0" encoding="utf-8"?>'); 
 
 include('config.php');
 include('include/functions.php');
