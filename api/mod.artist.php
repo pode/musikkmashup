4b103dd9220d5bb2a389942599ba517e061ba12d
Magnus Enger <magnus@torfeus.deich.folkebibl.no>
2009-10-22 11:12:38 +0200

    Fjerner 'ledetekster' fra bokser for album og artist.

--- api/mod.artist.php
+++ api/mod.artist.php
@@ -20,12 +20,12 @@ if (!empty($_GET['artist'])) {
 		'artist' => avinverter($_GET['artist'])
 	);
 	if ( $art = $artist->getinfo($methodVars) ) {
-		echo("<p>Navn: {$art['name']}</p>");
+		echo("<p>{$art['name']}</p>");
 		if ($art['image']['large']) {
 			echo('<p class="artistbilde"><img src="' . $art['image']['large'] . '" alt="' . $art['name'] . '" title="' . $art['name'] . '" /></p>');
 		}
 		if ($art['bio']['summary']) {
-			echo("<p>Biografi fra Last.fm: " . lastfm_lenker($art['bio']['summary']) . "</p>");
+			echo('<p>' . lastfm_lenker($art['bio']['summary']) . '</p>');
 		}
 		echo("<p>Lignende artister:</p><ul>");
 		foreach ($art['similar'] as $sim) {
